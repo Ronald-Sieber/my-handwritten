fc83002420f39c133e176ad462d1f73c5c8e39a1
Ronald-Sieber <chizha0226@163.com>
2026-03-29 15:30:23 +0800

    fix: 添加注释以说明对传入值的处理逻辑

--- reactive/reactive.js
+++ reactive/reactive.js
@@ -4,6 +4,7 @@ import { isObject } from './utils.js'
 //缓存代理对象，防止重复代理
 const proxyMap = new Map()
 export function reactive(target) {
+  // 对传入值的三种类型进行处理
   if (!isObject(target)) return target
 
   if (proxyMap.has(target)) return proxyMap.get(target)
